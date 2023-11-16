cd4d91979d23d234b030c92d5b29f8d0dc6b7c51
Szymon-Winiecki <Szymon.win4@gmail.com>
2023-11-16 19:29:29 +0100

    fix time formatting

--- util/formatingUtils.js
+++ util/formatingUtils.js
@@ -1,7 +1,10 @@
 
 export function timestampToString(timestamp){
     const date = new Date(timestamp);
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const h = date.getHours().toString().padStart(2, '0');
+    const min = date.getMinutes().toString().padStart(2, '0');
+    const sec = date.getSeconds().toString().padStart(2, '0');
+    return `${h}:${min}:${sec}`;
 }
 
 export function limitStringLength(string, maxLength, postfix="..."){
